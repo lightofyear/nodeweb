763ec04f29b7a415d2afe5c1217909baeea5b88a
lightofyear <37588871+lightofyear@users.noreply.github.com>
2018-11-13 17:33:54 +0800

    Update llrouter.js

--- routers/llrouter.js
+++ routers/llrouter.js
@@ -5,14 +5,14 @@ const user = require('../control/user');//拿到user表的控制层
 router.get('/llin/',function (ctx) {
     ctx.body= {
         status:200,
-        msg: '大奶子琳琳的路由!'
+        msg: '琳琳的路由!'
     }
 });
 
 router.get('/llin/getnz',function (ctx) {
     ctx.body= {
         status:200,
-        msg: '大奶子琳琳的奶子是我的!'
+        msg: '琳琳的奶子是我的!'
     }
 });
 
